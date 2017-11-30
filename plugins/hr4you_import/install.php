8dd2da76569bfbbec465e1aaccccb48dbea61bd2
tobias <tobias@UbuntuXPS>
2017-12-01 00:16:44 +0100

    More safe install

--- plugins/hr4you_import/install.php
+++ plugins/hr4you_import/install.php
@@ -1,10 +1,22 @@
 <?php
 $sql = rex_sql::factory();
 
-$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs` ADD `hr4you_job_id` int(10) default NULL;');
-$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs` ADD `hr4you_url_application_form` varchar(255) default NULL;');
-$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs_lang` ADD `hr4you_lead_in` varchar(255) default NULL;');
-$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_categories` ADD `hr4you_category_id` int(10) default NULL;');
+$sql->setQuery("SHOW COLUMNS FROM ". \rex::getTablePrefix() ."d2u_jobs_jobs LIKE 'hr4you_job_id';");
+if($sql->getRows() == 0) {
+	$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs` ADD `hr4you_job_id` int(10) default NULL;');
+}
+$sql->setQuery("SHOW COLUMNS FROM ". \rex::getTablePrefix() ."d2u_jobs_jobs LIKE 'hr4you_url_application_form';");
+if($sql->getRows() == 0) {
+	$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs` ADD `hr4you_url_application_form` varchar(255) default NULL;');
+}
+$sql->setQuery("SHOW COLUMNS FROM ". \rex::getTablePrefix() ."d2u_jobs_jobs_lang LIKE 'hr4you_lead_in';");
+if($sql->getRows() == 0) {
+	$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_jobs_lang` ADD `hr4you_lead_in` varchar(255) default NULL;');
+}
+$sql->setQuery("SHOW COLUMNS FROM ". \rex::getTablePrefix() ."d2u_jobs_categories LIKE 'hr4you_category_id';");
+if($sql->getRows() == 0) {
+	$sql->setQuery('ALTER TABLE `'. rex::getTablePrefix() . 'd2u_jobs_categories` ADD `hr4you_category_id` int(10) default NULL;');
+}
 
 // Insert frontend translations
 if(class_exists(d2u_jobs_hr4you_lang_helper)) {
