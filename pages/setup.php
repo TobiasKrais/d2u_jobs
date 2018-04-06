bb15807c37b0623d22d2afb78a92a532f043e083
tobias <tobias@UbuntuXPS>
2018-04-07 00:09:27 +0200

    Import from old Redaxo 4 tables

--- pages/setup.php
+++ pages/setup.php
@@ -15,6 +15,101 @@ if($d2u_module_id != "") {
 // D2UModuleManager show list
 $d2u_module_manager->showManagerList();
 
+// Import from Redaxo 4 D2U Stellenmarkt
+$sql = rex_sql::factory();
+$sql->setQuery("SHOW TABLES LIKE '". rex::getTablePrefix() ."d2u_stellenmarkt_stellen'");
+$old_tables_available = $sql->getRows() > 0 ? TRUE : FALSE;
+if(rex_request('import', 'string') == "d2u_stellenmarkt" && $old_tables_available) {
+	$sql->setQuery("DROP TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs`;		
+			DROP TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang`;		
+			DROP TABLE `". rex::getTablePrefix() ."d2u_jobs_categories`;		
+			DROP TABLE `". rex::getTablePrefix() ."d2u_jobs_categories_lang`;		
+			DROP TABLE `". rex::getTablePrefix() ."d2u_jobs_contacts`;");
+	$sql->setQuery("RENAME TABLE `". rex::getTablePrefix() ."d2u_stellenmarkt_stellen` TO `". rex::getTablePrefix() ."d2u_jobs_jobs`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `stellen_id` `job_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `interne_nummer` `reference_number` INT(10) NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `kategorie_ids` `category_ids` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `datum` `date` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `ort` `city` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `bild` `picture` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `status` `online_status` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'online';
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `kontakt_id` `contact_id` INT(10) NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs`
+			DROP `interne_bezeichnung`,
+			DROP `artikel_id`;
+
+		RENAME TABLE `". rex::getTablePrefix() ."d2u_stellenmarkt_stellen_lang` TO `". rex::getTablePrefix() ."d2u_jobs_jobs_lang`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `stellen_id` `job_id` INT(11) UNSIGNED NOT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` DROP PRIMARY KEY;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` SET `clang_id` = (`clang_id` + 1);
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` ADD PRIMARY KEY (`job_id`,`clang_id`);
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `bezeichnung` `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `aufgaben_ueberschrift` `tasks_heading` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `aufgaben_text` `tasks_text` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `profil_ueberschrift` `profile_heading` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `profil_text` `profile_text` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `angebot_ueberschrift` `offer_heading` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `angebot_text` `offer_text` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` ADD `translation_needs_update` VARCHAR(7) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `offer_text`;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` SET `translation_needs_update` = 'no';
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` ADD `updatedate` INT(11) NULL DEFAULT NULL;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` SET `updatedate` = UNIX_TIMESTAMP();
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` ADD `updateuser` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+		RENAME TABLE `". rex::getTablePrefix() ."d2u_stellenmarkt_kategorien` TO `". rex::getTablePrefix() ."d2u_jobs_categories`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories` CHANGE `kategorie_id` `category_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories` ADD `priority` INT(10) NULL DEFAULT NULL AFTER `category_id`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories` ADD `picture` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `priority`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories` DROP `interne_bezeichnung`;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_categories` SET `priority` = 1;
+
+		RENAME TABLE `". rex::getTablePrefix() ."d2u_stellenmarkt_kategorien_lang` TO `". rex::getTablePrefix() ."d2u_jobs_categories_lang`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` CHANGE `kategorie_id` `category_id` INT(11) UNSIGNED NOT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` DROP PRIMARY KEY;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` SET `clang_id` = (`clang_id` + 1);
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` ADD PRIMARY KEY (`category_id`,`clang_id`);
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` ADD `translation_needs_update` VARCHAR(7) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `name`;
+		UPDATE `". rex::getTablePrefix() ."d2u_jobs_categories_lang` SET `translation_needs_update` = 'no';
+
+		RENAME TABLE `". rex::getTablePrefix() ."d2u_stellenmarkt_kontakt` TO `". rex::getTablePrefix() ."d2u_jobs_contacts`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_contacts` CHANGE `kontakt_id` `contact_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_contacts` CHANGE `bild` `picture` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_contacts` CHANGE `telefon` `phone` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;");
+
+	$error = $sql->hasError() ? $sql->getError() : "";
+	
+	$sql->setQuery("DROP FUNCTION IF EXISTS `getPriority`;");
+	$sql->setQuery("delimiter $$
+		CREATE FUNCTION `getPriority`() RETURNS int(11) DETERMINISTIC
+		begin
+			return if(@jobCatPrio, @jobCatPrio:=@jobCatPrio+1, @jobCatPrio:=1);
+		end$$
+		delimiter ;");
+	$sql->setQuery("UPDATE ". rex::getTablePrefix() ."d2u_jobs_categories SET priority = getPriority() ORDER BY `priority`, `category_id`;
+		DROP FUNCTION IF EXISTS `getPriority`;");
+
+	$sql->setQuery("SHOW COLUMNS FROM ". \rex::getTablePrefix() ."d2u_jobs_jobs LIKE 'hr4you_jobid';");
+	if($sql->getRows() > 0 && $error == "") {
+		$sql->setQuery("ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` CHANGE `hr4you_jobid` `hr4you_job_id` INT(10) NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs` ADD `hr4you_url_application_form` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `hr4you_job_id`;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_jobs_lang` CHANGE `hr4you_einleitung` `hr4you_lead_in` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+		ALTER TABLE `". rex::getTablePrefix() ."d2u_jobs_categories` CHANGE `hr4you_berufskategorie_id` `hr4you_category_id` INT(10) NULL DEFAULT NULL;");
+		$error = $sql->hasError() ? $sql->getError() : "";
+	}
+
+	if($error != "") {
+		print rex_view::error('Fehler beim Import: '. $error);
+	}
+	else {
+		print rex_view::success('Daten aus Redaxo 4 D2U Stellenmarkt Addon importiert und alte Tabellen gelöscht');
+	}
+}
+else if($old_tables_available) {
+	print "<h2>Import aus Redaxo 4 D2U Stellenmarkt Addon</h2>";
+	print "<p>Es wurden die D2U Stellenmarkt Addon Tabellen aus Redaxo 4 in der Datenbank gefunden."
+	. "Sollen die Daten importiert werden und die alten Tabellen gelöscht werden? ACHTUNG: dabei werden alle vorhandenen Daten gelöscht!</p>";
+	print '<a href="'. rex_url::currentBackendPage(["import" => "d2u_stellenmarkt"], FALSE) .'"><button class="btn btn-save">Import und vorhandene Daten löschen</button></a>';
+}
 /*
  * Templates
  */
@@ -31,6 +126,7 @@ $d2u_module_manager->showManagerList();
 <h2>Changelog</h2>
 <p>1.0.4-DEV:</p>
 <ul>
+	<li>Datenübernahme aus Redaxo 4 D2U Stellenmarkt Addon möglich. Dazu müssen lediglich die entsprechenden Tabellen des alten Addons in der Datenbank vorhanden sein.</li>
 	<li>Bugfix: Löschen von Sprachen schlug fehl.</li>
 	<li>Bugfix: Fehler beim Speichern von Namen mit einfachem Anführungszeichen behoben.</li>
 </ul>
