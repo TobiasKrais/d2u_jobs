02cfdc5ce1cc5c7e98f1df309306646057c84b4a
tobias <tobias@UbuntuXPS>
2018-04-24 23:11:02 +0200

    Warning removed

--- plugins/hr4you_import/lib/hr4you.php
+++ plugins/hr4you_import/lib/hr4you.php
@@ -7,6 +7,11 @@ class hr4you {
 	 * Perform HR4You XML import, calls import()
 	 */
 	public static function autoimport() {
+		// Include mediapool functions when call is frontend call
+		if (!rex::isBackend()) {
+			require_once __DIR__ . '/../../../../mediapool/functions/function_rex_mediapool.php';
+		}
+
 		if(self::import()) {
 			print \rex_view::success(\rex_i18n::msg('d2u_jobs_hr4you_import_success'));
 		}
@@ -17,11 +22,6 @@ class hr4you {
 	 * @return boolean TRUE if successfull
 	 */
 	public static function import() {
-		// Include mediapool functions when call is frontend call
-		if (!rex::isBackend()) {
-			require_once __DIR__ . '/../../../../mediapool/functions/function_rex_mediapool.php'; 
-		}
-		
 		$hr4you_xml_url = \rex_config::get('d2u_jobs', 'hr4you_xml_url', FALSE);
 		if($hr4you_xml_url === FALSE) {
 			print \rex_view::error(\rex_i18n::msg('d2u_jobs_hr4you_settings_failure_xml_url'));
@@ -36,6 +36,7 @@ class hr4you {
 		}
 		$xml_jobs = new SimpleXMLElement($xml_contents);
 
+		self::log('***** Starting Import *****');
 		// Get old stuff to be able to delete it later
 		$old_jobs = \D2U_Jobs\Job::getAllHR4YouJobs();
 		$old_contacts = []; // Get them later from Jobs
@@ -67,11 +68,17 @@ class hr4you {
 					if(in_array($job_picture->getFileName(), $old_pictures)) {
 						unset($old_pictures[$job_picture->getFileName()]);
 					}
+					self::log('Job picture '. $job_picture_filename .' already available in mediapool.');
 				}
 				else {
 					// File exists only in database, but no more physically: remove it before import
 					if($job_picture instanceof \rex_media) {
-						\rex_mediapool_deleteMedia($job_picture->getFileName());
+						try {
+							\rex_mediapool_deleteMedia($job_picture->getFileName());
+						}
+						catch(Exception $e) {
+							self::log('Picture physically not found. Error deleting media from mediapool database.');
+						}
 					}
 					
 					// Import
@@ -79,8 +86,11 @@ class hr4you {
 					// Copy first
 					if(copy($xml_job->kopfgrafik_url, $target_picture)) {
 						chmod($target_picture, octdec(664));
-						$sync_file_infos = \rex_mediapool_syncFile($job_picture_pathInfo['basename'], \rex_config::get('d2u_jobs', 'hr4you_media_category'), $xml_jobs->titel);
+// Use next line instead the one after as soon as https://github.com/redaxo/redaxo/issues/1614 is fixed
+//						$sync_file_infos = \rex_mediapool_syncFile($job_picture_pathInfo['basename'], \rex_config::get('d2u_jobs', 'hr4you_media_category'), $xml_jobs->titel);
+						$sync_file_infos = self::importMedia($job_picture_pathInfo['basename'], \rex_config::get('d2u_jobs', 'hr4you_media_category'), $xml_jobs->titel);
 						$job_picture_filename = $sync_file_infos['filename'];
+						self::log('Job picture '. $job_picture_filename .' importet into database.');
 					}
 				}
 			}
@@ -100,6 +110,10 @@ class hr4you {
 			$contact->save();
 			if(array_key_exists($contact->contact_id, $old_contacts)) {
 				unset($old_contacts[$contact->contact_id]);
+				self::log('Contact '. $contact->name .' already exists.');
+			}
+			else {
+				self::log('Contact '. $contact->name .' added.');
 			}
 			if($contact->picture != "" && in_array($contact->picture, $old_pictures)) {
 				unset($old_pictures[$contact->picture]);
@@ -108,6 +122,7 @@ class hr4you {
 			// Category
 			$category = \D2U_Jobs\Category::getByHR4YouID($xml_job->berufskategorie_id->__toString());
 			if($category === FALSE) {
+				self::log('Category with HR4You ID '. $xml_job->berufskategorie_id->__toString() .' does not exist. Falback to default category.');
 				$category = new \D2U_Jobs\Category(\rex_config::get('d2u_jobs', 'hr4you_default_category'), \rex_config::get('d2u_jobs', 'hr4you_default_lang'));
 			}
 
@@ -152,17 +167,23 @@ class hr4you {
 
 			if(array_key_exists($job->hr4you_job_id, $old_jobs)) {
 				unset($old_jobs[$job->hr4you_job_id]);
+				self::log('Job '. $job->name .' already exists. Updated.');
+			}
+			else {
+				self::log('Job '. $job->name .' added.');
 			}
 		}
 
 		// Delete unused old jobs
 		foreach ($old_jobs as $old_job) {
 			$old_job->delete(TRUE);
+			self::log('Job '. $old_job->name .' deleted.');
 		}
 		
 		// Delete unused old contacts
 		foreach ($old_contacts as $old_contact) {
 			$old_contact->delete();
+			self::log('Contact '. $old_contact->name .' deleted.');
 		}
 
 		// Delete unused old pictures
@@ -170,6 +191,10 @@ class hr4you {
 			$delete_result = \rex_mediapool_deleteMedia($old_picture);
 			if($delete_result['ok'] === FALSE) {
 				// File seems to be in use
+				self::log('Picture '. $old_picture .' deletion requested, but is in use.');
+			}
+			else {
+				self::log('Picture '. $old_picture .' deleted.');
 			}
 		}
 		
@@ -227,6 +252,123 @@ class hr4you {
 	}
 
 	/**
+	 * Synchronisiert die Datei $physical_filename des Mediafolders in den
+	 * Medienpool.
+	 *
+	 * @param string $physical_filename
+	 * @param int    $rex_file_category
+	 * @param string $title
+	 *
+	 * @return bool|array
+	 */
+	private static function importMedia($physical_filename, $rex_file_category, $title) {
+		$abs_file = \rex_path::media($physical_filename);
+
+		if (!file_exists($abs_file)) {
+			return FALSE;
+		}
+
+		$filesize = filesize($abs_file);
+		$filetype = "";
+		if (function_exists('mime_content_type')) {
+			$filetype = mime_content_type($abs_file);
+		}
+		else if (function_exists('finfo_open')) {
+			$finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+			$filetype = finfo_file($finfo, $abs_file);
+		}
+
+		$FILE = [];
+		$FILE['name'] = $physical_filename;
+		$FILE['size'] = $filesize;
+		$FILE['type'] = $filetype;
+
+		$FILEINFOS = [];
+		$FILEINFOS['title'] = $title;
+
+		// Check file category exists
+		$gc = rex_sql::factory();
+		$gc->setQuery('SELECT * FROM ' . \rex::getTablePrefix() . 'media_category WHERE id=' . $rex_file_category);
+		if ($gc->getRows() != 1) {
+			$rex_file_category = 0;
+		}
+
+		$FILENAME = $FILE['name'];
+		$FILESIZE = $FILE['size'];
+		$FILETYPE = $FILE['type'];
+		$NFILENAME = \rex_mediapool_filename($FILENAME, TRUE);
+		$message = [];
+
+		// ----- alter/neuer filename
+		$srcFile = \rex_path::media($FILENAME);
+		$dstFile = \rex_path::media($NFILENAME);
+
+		$success = true;
+		// Filesync
+		if (!@rename($srcFile, $dstFile)) {
+			$message[] = \rex_i18n::msg('pool_file_movefailed');
+			$success = false;
+		}
+
+		if ($success) {
+			@chmod($dstFile, \rex::getFilePerm());
+
+			// get width height
+			$size = @getimagesize($dstFile);
+
+			if ($FILETYPE == '' && isset($size['mime'])) {
+				$FILETYPE = $size['mime'];
+			}
+
+			$FILESQL = \rex_sql::factory();
+			$FILESQL->setTable(\rex::getTablePrefix() . 'media');
+			$FILESQL->setValue('filetype', $FILETYPE);
+			$FILESQL->setValue('title', $FILEINFOS['title']);
+			$FILESQL->setValue('filename', $NFILENAME);
+			$FILESQL->setValue('originalname', $FILENAME);
+			$FILESQL->setValue('filesize', $FILESIZE);
+
+			if ($size) {
+				$FILESQL->setValue('width', $size[0]);
+				$FILESQL->setValue('height', $size[1]);
+			}
+
+			$FILESQL->setValue('category_id', $rex_file_category);
+			$FILESQL->setDateTimeValue('createdate', time());
+			$FILESQL->setDateTimeValue('createuser', null);
+			$FILESQL->setDateTimeValue('updatedate', time());
+			$FILESQL->setDateTimeValue('updateuser', null);
+			$FILESQL->insert();
+
+			if ($NFILENAME != $FILENAME) {
+				$message[] = \rex_i18n::rawMsg('pool_file_renamed', $FILENAME, $NFILENAME);
+			}
+
+			\rex_media_cache::deleteList($rex_file_category);
+		}
+
+		$RETURN['title'] = $FILEINFOS['title'];
+		$RETURN['type'] = $FILETYPE;
+		$RETURN['msg'] = implode('<br />', $message);
+		// Aus BC gruenden hier mit int 1/0
+		$RETURN['ok'] = $success ? 1 : 0;
+		$RETURN['filename'] = $NFILENAME;
+		$RETURN['old_filename'] = $FILENAME;
+
+		if (isset($size)) {
+			$RETURN['width'] = $size[0];
+			$RETURN['height'] = $size[1];
+		}
+
+		// ----- EXTENSION POINT
+		if ($success) {
+			\rex_extension::registerPoint(new \rex_extension_point('MEDIA_ADDED', '', $RETURN));
+		}
+
+		return $RETURN;
+	}
+	
+	/**
 	 * Removes not allowed tags and other stuff from string.
 	 * @param string $string String to be prepared
 	 * @return string Prepared string
@@ -237,5 +379,21 @@ class hr4you {
 		$string = str_replace(['&nbsp;', '&crarr;'], ' ', $string);
 		$string = preg_replace("/\s+/", " ", $string); 
 		return str_replace(["\r", "\n"], '', $string);
+	}
+	
+	/**
+	 * Logs message
+	 * @param string $message Message to be logged
+	 */
+	private static function log($message) {
+		$log = file_exists(rex_path::addonCache('d2u_jobs', 'hr4you_import_log.txt')) ? file_get_contents(rex_path::addonCache('d2u_jobs', 'hr4you_import_log.txt')) : "";
+		
+		$log .= PHP_EOL. date('d.m.Y h:i:s', time()) .": ". $message;
+
+		// Write to log
+		if(!is_dir(rex_path::addonCache('d2u_jobs'))) {
+			mkdir(rex_path::addonCache('d2u_jobs'), 0755, TRUE);
+		}
+		file_put_contents(rex_path::addonCache('d2u_jobs', 'hr4you_import_log.txt'), $log);
 	}
 }
