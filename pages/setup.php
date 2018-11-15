1d15f956bfce1befc55260f3a2de4725e1168823
tobias <tobias@UbuntuXPS>
2018-11-15 16:19:15 +0100

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -129,6 +129,10 @@ else if($old_tables_available) {
 <p>Fehlermeldungen bitte im Git Projekt unter
 	<a href="https://github.com/TobiasKrais/d2u_jobs/issues" target="_blank">https://github.com/TobiasKrais/d2u_jobs/issues</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.0.7-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.0.6:</p>
 <ul>
 	<li>Bugfix: Deaktiviertes Addon zu deinstallieren führte zu fatal error.</li>
