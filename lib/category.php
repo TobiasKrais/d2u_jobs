73ec4d5700ea217c0b033f287e866d574aeb849a
tobias <tobias@UbuntuXPS>
2019-01-21 17:26:24 +0100

    reorder priorities after deletion

--- lib/category.php
+++ lib/category.php
@@ -94,6 +94,9 @@ class Category implements \D2U_Helper\ITranslationHelper {
 				."WHERE category_id = ". $this->category_id;
 			$result = \rex_sql::factory();
 			$result->setQuery($query);
+
+			// reset priorities
+			$this->setPriority(TRUE);			
 		}
 	}
 	
@@ -343,9 +346,10 @@ class Category implements \D2U_Helper\ITranslationHelper {
 	}
 	
 	/**
-	 * Reassigns priority to all Categories in database.
+	 * Reassigns priorities in database.
+	 * @param boolean $delete Reorder priority after deletion
 	 */
-	private function setPriority() {
+	private function setPriority($delete = FALSE) {
 		// Pull prios from database
 		$query = "SELECT category_id, priority FROM ". \rex::getTablePrefix() ."d2u_jobs_categories "
 			."WHERE category_id <> ". $this->category_id ." ORDER BY priority";
@@ -357,8 +361,8 @@ class Category implements \D2U_Helper\ITranslationHelper {
 			$this->priority = 1;
 		}
 		
-		// When prio is too high, simply add at end 
-		if($this->priority > $result->getRows()) {
+		// When prio is too high or was deleted, simply add at end 
+		if($this->priority > $result->getRows() || $delete) {
 			$this->priority = $result->getRows() + 1;
 		}
 
--- pages/contact.php
+++ pages/contact.php
@@ -109,7 +109,7 @@ if ($func == 'edit' || $func == 'add') {
 		</div>
 	</form>
 	<br>
-	<script type='text/javascript'>
+	<script>
 		jQuery(document).ready(function($) {
 			$('legend').each(function() {
 				$(this).addClass('open');
