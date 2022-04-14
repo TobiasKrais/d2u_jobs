248db31776700b4a617351f1e6be3a886ec34f7d
tobias <tobias@UbuntuXPS>
2022-04-14 11:27:41 +0200

    prepare 1.2.3

--- pages/setup.php
+++ pages/setup.php
@@ -129,6 +129,10 @@ else if($old_tables_available) {
 <p>Fehlermeldungen bitte im Git Projekt unter
 	<a href="https://github.com/TobiasKrais/d2u_jobs/issues" target="_blank">https://github.com/TobiasKrais/d2u_jobs/issues</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.2.3-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.2.2:</p>
 <ul>
 	<li>Anpassungen an Publish Github Release to Redaxo.</li>
