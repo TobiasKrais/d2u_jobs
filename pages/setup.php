f506788a5d2c359253744ad9a5793a5a64944cb5
tobias <tobias@UbuntuXPS>
2022-06-03 16:22:40 +0200

    ignore .github

--- pages/setup.php
+++ pages/setup.php
@@ -131,6 +131,7 @@ else if($old_tables_available) {
 <h2>Changelog</h2>
 <p>1.2.3-DEV:</p>
 <ul>
+	<li>.github Verzeichnis aus Installer Action ausgeschlossen.</li>
 	<li>Bugfix hr4you Import: Bilder wurden erst beim 2. Import mit der Stellenanzeige verknüpft.</li>
 </ul>
 <p>1.2.2:</p>
