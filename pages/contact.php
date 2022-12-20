0f90ee7e6b1cd7b15bb10ec1b993e6417b70e5ae
tobias <tobias@UbuntuXPS>
2022-12-20 17:22:44 +0100

    rexstan

--- pages/contact.php
+++ pages/contact.php
@@ -9,7 +9,7 @@ if($message != "") {
 }
 
 // save settings
-if (filter_input(INPUT_POST, "btn_save") == 1 || filter_input(INPUT_POST, "btn_apply") == 1) {
+if (intval(filter_input(INPUT_POST, "btn_save")) === 1 || intval(filter_input(INPUT_POST, "btn_apply")) === 1) {
 	$form = (array) rex_post('form', 'array', []);
 
 	// Media fields and links need special treatment
